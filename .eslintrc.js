f2ff72f9820b8c1ace427084af42e4829c40f6ca
Tuan Nguyen <>
2025-08-01 19:23:13 +0700

    chore: add legacy ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist/**', 'node_modules/**', 'jest.config.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'eqeqeq': 'error',
+    'curly': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.ts', 'test/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
+};
